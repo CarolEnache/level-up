9d10aa8357baa528041d362ee6448f0fa449b323
Carol Enache <enache.carol@gmail.com>
2018-10-02 22:55:34 +0100

    make use of 'render props children' and make toggle reusable

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import ToggleRenderProps from './ToggleRenderProps'
+import ToggleRPC from './ToggleRPC';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ToggleRenderProps render={({on, toggle}) => (
-          <div>
-            {on &&<h1>Hello World</h1>}
-            <button onClick={toggle}>Show/Hidea</button>
-          </div>
-        )}
-        
-        />
+        <ToggleRPC >
+          {({on, toggle}) => (
+            <div>
+              {on && <h1>Hello World</h1>}
+              <button onClick={toggle}>Show/Hidea</button>
+            </div>
+          )}
+        </ToggleRPC>
       </div>
     );
   }
