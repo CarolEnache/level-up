402bb20d99e4a21ed1976c280ae4dacf78f538e3
Carol Enache <enache.carol@gmail.com>
2018-10-02 23:27:13 +0100

    Create reuseable Portal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import ToggleRPC from './ToggleRPC';
+import Portal from './Portal';
 
 class App extends Component {
   render() {
@@ -11,6 +12,9 @@ class App extends Component {
             <Fragment>
               {on && <h1>Hello World</h1>}
               <button onClick={toggle}>Show/Hidea</button>
+              <Portal >
+                {on &&<h1>Hi I'm in a portal</h1>}
+              </Portal>
             </Fragment>
           )}
         </ToggleRPC>
