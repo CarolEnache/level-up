efdc2c87705f3e8da4e8de9bc07b128514de2489
Carol Enache <enache.carol@gmail.com>
2018-10-03 12:41:24 +0100

    Create a re-useable Modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import ToggleRPC from './ToggleRPC';
 import Portal from './Portal';
+import Modal from './Modal';
 
 class App extends Component {
   render() {
@@ -10,11 +11,10 @@ class App extends Component {
         <ToggleRPC >
           {({on, toggle}) => (
             <Fragment>
-              {on && <h1>Hello World</h1>}
-              <button onClick={toggle}>Show/Hidea</button>
-              <Portal >
-                {on &&<h1>Hi I'm in a portal</h1>}
-              </Portal>
+              <button onClick={toggle}>Login</button>
+              <Modal on={on} toggle={toggle}>
+                <h1>Still in Modal</h1>
+              </Modal>
             </Fragment>
           )}
         </ToggleRPC>
