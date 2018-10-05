f8a4f4f005d55972ba0d7740c973caaee0ddb81d
Carol Enache <enache.carol@gmail.com>
2018-10-05 13:13:41 +0100

    use the Context API to send functionality as well as data

--- src/User.js
+++ src/User.js
@@ -10,6 +10,7 @@ export default class User extends Component {
                 <h1>User info:</h1>
                 <h3>{context.user.name}</h3>
                 <h3>{context.user.email}</h3>
+                <button onClick={context.logout}>Logout</button>
             </div>        
         )}
         </UserContext.Consumer>
