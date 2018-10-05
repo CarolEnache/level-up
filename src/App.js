6d73e42fa8037538f2de0f2b89050483c28d1f08
Carol Enache <enache.carol@gmail.com>
2018-10-05 13:04:08 +0100

    React Context Api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, createContext } from 'react';
 import './App.css';
 import { Toggle } from 'Utilities';
 import { Modal } from 'Elements';
+import User from './User';
+import { UserContext } from './UserContext';
  
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'Carol',
+    email: "enache.carol@gmail.com"
+  }
+
+  render(){
+    return(
+      <UserContext.Provider 
+        value={{
+          user: this.state
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    ) 
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Toggle >
-          {({on, toggle}) => (
-            <Fragment>
-              <button onClick={toggle}>Login</button>
-              <Modal on={on} toggle={toggle}>
-                <h1>Still in Modal</h1>
-              </Modal>
-            </Fragment>
-          )}
-        </Toggle>
-      </div>
+      <UserProvider>
+        <div className="App">
+          <User />
+          <Toggle >
+            {({on, toggle}) => (
+              <Fragment>
+                <button onClick={toggle}>Login</button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>Still in Modal</h1>
+                </Modal>
+              </Fragment>
+            )}
+          </Toggle>
+        </div>      
+      </UserProvider>
     );
   }
 }
