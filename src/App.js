8a8c0f40aeb1001df89b478f35aa31f2087df4e0
Carol Enache <enache.carol@gmail.com>
2018-10-05 16:18:56 +0100

    Animated  draggable components

--- src/App.js
+++ src/App.js
@@ -3,31 +3,16 @@ import { Transition } from  'react-spring';
 
 import './App.css';
 import { Toggle } from 'Utilities';
-import { Modal, Card } from 'Elements';
-import User from './User';
+import { Modal } from 'Elements';
 import UserProvider from './UserProvider';
+import Drag from './Drag';
  
 class App extends Component {
   render() {
     return (
       <UserProvider>
         <div className="App">
-          <User />
-          <section>
-            <Toggle >
-              {({on, toggle}) => (
-                <Fragment>
-                  <button onClick={toggle}>Show / Hide</button>
-                  <Transition
-                    from={{ opacity: 0, bg: '#82d8d8', height: '0px'}} 
-                    enter={{ opacity: 1, bg: '#524763', height: '200px'}} 
-                    leave={{ opacity: 0, bg: '#82d8d8', height: '0px'}}>
-                    { on && Header }
-                  </Transition>
-                </Fragment>
-              )}
-            </Toggle>
-          </section>
+          <Drag />
           <Toggle >
             {({on, toggle}) => (
               <Fragment>
@@ -44,17 +29,5 @@ class App extends Component {
   }
 }
 
-const Header = styles => (
-  <Card style={{ 
-    opacity: styles.opacity,
-    background: styles.bg,
-    overflow: 'hidden',
-    height: styles.height
-  }}>
-    <h1 >Show me</h1>
-    <h3>{styles.bg}</h3>
-  </Card>
-
-  );
 
 export default App;
--- src/Elements/Cards.js
+++ src/Elements/Cards.js
@@ -4,7 +4,10 @@ import { transition, elevation, colors } from 'Utilities';
 export const Card = styled.div`
   background: white;
   border-radius: 5px;
-  padding: 15px
+  padding: 15px;
+  max-width: 320px;
+  margin: 0 auto;
+  margin-top: 100px;
   color: ${colors.black};
   ${elevation[4]};
   ${transition({
